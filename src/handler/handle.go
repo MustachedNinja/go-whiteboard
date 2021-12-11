84fb7b080dfb1d3df451f983dfebe372788d7fa8
MustachedNinja <kostya.yatsuk9@live.com>
2021-12-10 17:32:55 -0800

    Add helper functions to switch between returning data as JSON vs Proto

--- src/handler/handle.go
+++ src/handler/handle.go
@@ -6,17 +6,36 @@ import (
 	"net/http"
 	"whiteboard/state"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
+
+func MarshalProto(protoObj proto.Message, asJson bool) ([]byte, error) {
+	if asJson {
+		jsonBytes, err := protojson.MarshalOptions{}.Marshal(protoObj)
+		return jsonBytes, err
+	} else {
+		return proto.Marshal(protoObj)
+	}
+}
+
+func WriteResponse(w http.ResponseWriter, data []byte, asJson bool) {
+	if asJson {
+		fmt.Fprint(w, string(data))
+	} else {
+		fmt.Fprint(w, data)
+	}
+}
 
 func GetUpdates(w http.ResponseWriter, req *http.Request) {
 	update := state.StateUpdate{
 		Version: "abra",
 		Data1:   1234,
 	}
-	out, err := proto.Marshal(&update)
+
+	out, err := MarshalProto(&update, true)
 	if err != nil {
 		log.Fatalf("Marshaling error: ", err)
 	}
-	fmt.Fprint(w, out)
+	WriteResponse(w, out, true)
 }
